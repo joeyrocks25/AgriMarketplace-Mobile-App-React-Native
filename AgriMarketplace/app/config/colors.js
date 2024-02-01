d7df23465c23597656a1eb2582270f1971c855cf
kp <mackin153@hotmail.com>
2024-02-01 03:56:33 +0000

    Recommendations for haulers if view livestock and improve listingdetailsscreen

--- AgriMarketplace/app/config/colors.js
+++ AgriMarketplace/app/config/colors.js
@@ -12,12 +12,12 @@ export default {
   dark_orange: "#e57e26",
   light_green: "#b4b937",
   dark_green: "#80b741",
-  livestock: "#fc5c65",   
-  machinery: "#fd9644",  
-  tractors: "#fed330", 
-  cars: "#26de81",   
-  tools: "#2bcbba",        
+  livestock: "#fc5c65",
+  machinery: "#fd9644",
+  tractors: "#fed330",
+  cars: "#26de81",
+  tools: "#2bcbba",
   other: "#778ca3",
   test: "#F5F5F5",
   test2: "#D3D3D3",
-}
+};
