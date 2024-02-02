0a7d4a93daf1c476fc094f32678178a5e4bfe56f
kp <mackin153@hotmail.com>
2024-02-02 13:27:31 +0000

    updating haulers

--- AgriMarketplace/app/screens/HaulersScreen.js
+++ AgriMarketplace/app/screens/HaulersScreen.js
@@ -1,23 +1,80 @@
-import React, { useEffect, useRef } from "react";
-import { View, Text, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import { useIsFocused } from "@react-navigation/native";
 
-function HaulersScreen({ navigation, onScreenFocus }) {
+const countiesData = [
+  { id: 1, name: "Antrim" },
+  { id: 2, name: "Down" },
+  { id: 3, name: "Armagh" },
+  { id: 4, name: "Derry" },
+  { id: 5, name: "Tyrone" },
+  { id: 6, name: "Fermanagh" },
+];
+
+const haulersData = [
+  { id: 1, name: "Hauler 1", county: "Antrim" },
+  { id: 2, name: "Hauler 2", county: "Down" },
+  { id: 3, name: "Hauler 3", county: "Armagh" },
+  { id: 4, name: "Hauler 4", county: "Derry" },
+  { id: 5, name: "Hauler 5", county: "Tyrone" },
+  { id: 6, name: "Hauler 6", county: "Fermanagh" },
+  // Add more haulers as needed
+];
+
+function HaulersScreen({ navigation, route }) {
   const isFocused = useIsFocused();
+  const [selectedCounty, setSelectedCounty] = useState(null);
+  const filteredHaulers = haulersData.filter(
+    (hauler) => !selectedCounty || hauler.county === selectedCounty
+  );
 
   useEffect(() => {
     if (isFocused) {
-      console.log("ListingDetailsScreen is focused1");
-      onScreenFocus && onScreenFocus(true);
+      console.log("HaulersScreen is focused");
+      route.params &&
+        route.params.onScreenFocus &&
+        route.params.onScreenFocus(true);
     } else {
-      console.log("ListingDetailsScreen is unfocused1");
-      onScreenFocus && onScreenFocus(false);
+      console.log("HaulersScreen is unfocused");
+      route.params &&
+        route.params.onScreenFocus &&
+        route.params.onScreenFocus(false);
     }
-  }, [isFocused, onScreenFocus]);
+  }, [isFocused, route.params]);
+
+  const handleCountySelect = (county) => {
+    setSelectedCounty(county);
+  };
 
   return (
     <View>
-      <Text>This is the haulers Screen. Replace this with your content.</Text>
+      <Text>To view haulers in your area. Please Select a county:</Text>
+      <Picker
+        selectedValue={selectedCounty}
+        onValueChange={(itemValue) => handleCountySelect(itemValue)}
+      >
+        <Picker.Item label="All Counties" value={null} />
+        {countiesData.map((county) => (
+          <Picker.Item
+            key={county.id}
+            label={county.name}
+            value={county.name}
+          />
+        ))}
+      </Picker>
+
+      <Text>Haulers in {selectedCounty || "all counties"}:</Text>
+      <FlatList
+        data={filteredHaulers}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+            {/* Add more hauler details as needed */}
+          </View>
+        )}
+      />
     </View>
   );
 }
